d2ab81e32a483b66ed58c529f82d528bc4801d7d
Kamikatsu62 <jaypeedelsocorro@gmail.com>
2024-08-15 09:52:36 +0800

    My pers repo

---
+++ src/javadelsocorro/AgeCalculator.java
@@ -0,0 +1,21 @@
+
+package javadelsocorro;
+
+import java.util.Scanner;
+
+public class AgeCalculator {
+    public void getAge(){
+    
+    Scanner input = new Scanner(System.in);
+        
+        String fname;
+        int byear;
+        
+        System.out.print("Enter Your name: ");
+        fname = input.nextLine();
+        System.out.print("Enter your birthyear: ");
+        byear = input.nextInt();
+        
+        System.out.println("Hello "+fname+" Your age is "+(2024-byear));
+    } 
+}
