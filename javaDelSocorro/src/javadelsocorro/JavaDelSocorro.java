dd3578a8981cdb973ee32186a8a41cf0957d33d5
Kamikatsu62 <jaypeedelsocorro@gmail.com>
2024-08-29 10:08:37 +0800

    Add files via upload

---
+++ javaDelSocorro/src/javadelsocorro/JavaDelSocorro.java
@@ -0,0 +1,16 @@
+
+package javadelsocorro;
+
+import java.util.Scanner;   
+
+public class JavaDelSocorro {
+
+    
+    public static void main(String[] args) {
+        
+        ReceiptClass r = new ReceiptClass();
+        r.receipt();
+        
+    }
+    
+}
